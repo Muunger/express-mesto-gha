723a551b53425a63e9cea9ac917c606ec6427672
Aleksei Mamontov <aleksey4209@gmail.com>
2023-12-09 21:07:36 +0300

    dotenv don't work test13

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const dotenv = require('dotenv').config()
 const routerUser = require('./routes/user')
 const routerCard = require('./routes/card')
 const bodyParser = require('body-parser')
 
 
-const { PORT, MONGO_URI } = process.env
+const { PORT = 3000 } = process.env
 
 const app = express()
 
@@ -20,7 +19,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-mongoose.connect(MONGO_URI)
+mongoose.connect("mongodb://127.0.0.1:27017/mestodb")
 
 app.use('/', routerUser)
 app.use('/', routerCard)
